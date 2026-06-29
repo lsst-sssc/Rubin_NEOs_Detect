4ff750b277175725e815a0f091720a6cb70d2051
Dante Garcia <007046891@coyote.csusb.edu>
2026-06-29 14:43:46 -0700

    Apparent magnitude tests with fixed expected values, all tests passing

--- neo_detect/app_mag.py
+++ neo_detect/app_mag.py
@@ -8,9 +8,7 @@ class Apparent_Magnitude:
 
     def apparent_magnitude(self, r: float, delta: float, phase_angle: float) -> float:
 
-        phase_angle_rad = math.radians(phase_angle)
-
-        phi = Phase_Function(phase_angle_rad, self.G).phase_function()
+        phi = Phase_Function(phase_angle, self.G).phase_function()
 
         m = self.H + 5 * math.log10(r * delta) - 2.5 * math.log10(phi)
 
--- tests/test_app_mag.py
+++ tests/test_app_mag.py
@@ -12,7 +12,7 @@ class Test_Apparent_Magnitude:
         assert m == pytest.approx(expected_m, abs = 0.1)
 
     def test_app_mag_15H_angle30(self):
-        expected_m = 15.7
+        expected_m = 16.3
 
         app_mag = Apparent_Magnitude(H = 15.0, G = 0.15)
         m = app_mag.apparent_magnitude(r = 1.0, delta = 1.0, phase_angle = 30.0)
@@ -21,7 +21,7 @@ class Test_Apparent_Magnitude:
 
 
     def test_app_mag_30H_angle30(self):
-        expected_m = 30.7
+        expected_m = 31.3
 
         app_mag = Apparent_Magnitude(H = 30.0, G = 0.15)
         m = app_mag.apparent_magnitude(r = 1.0, delta = 1.0, phase_angle = 30.0)
