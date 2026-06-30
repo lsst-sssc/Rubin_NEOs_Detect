a631ed0224e2a7c073ea1d93dc97b962c02c8c1b
Tim Lister <tlister@lco.global>
2026-06-30 16:51:07 +0100

    Make absolute magnitude (H) a class variable and initalize it in the constructor. Add doc string to abs_mag()

--- neo_detect/abs_mag.py
+++ neo_detect/abs_mag.py
@@ -1,11 +1,24 @@
 import math
 
 class Asteroid:
+    H = None
+
     def __init__(self, diameter: float , albedo: float = 0.14):
         self.diameter = diameter
         self.albedo = albedo
 
-    def abs_mag(self):
+        # Initialize H
+        self.H = self.abs_mag()
 
+    def abs_mag(self):
+        """
+        Converts diameter to H magnitude (using class variables self.diameter and self.albedo)
+        Reference: CNEOS Asteroid Size Estimator (https://cneos.jpl.nasa.gov/tools/ast_size_est.html)
+
+        Returns
+        -------
+        float
+            Absolute magnitude, H
+        """
         H = 15.618 - 5 * math.log10(self.diameter / 1000.0) - 2.5 * math.log10(self.albedo)
-        return H 
+        return H
