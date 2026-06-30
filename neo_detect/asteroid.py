3e4d058c9c092fc241d90cfa2b169e486d8158b1
Dante Garcia <007046891@coyote.csusb.edu>
2026-06-30 10:53:35 -0700

    Moved all classes into asteroid class and confirmed tests work

--- neo_detect/asteroid.py
+++ neo_detect/asteroid.py
@@ -3,9 +3,10 @@ import math
 class Asteroid:
     H = None
 
-    def __init__(self, diameter: float , albedo: float = 0.14):
+    def __init__(self, diameter: float, albedo: float = 0.14, G: float = 0.15):
         self.diameter = diameter
         self.albedo = albedo
+        self.G = G
 
         # Initialize H
         self.H = self.abs_mag()
@@ -20,5 +21,25 @@ class Asteroid:
         float
             Absolute magnitude, H
         """
-        H = 15.618 - 5 * math.log10(self.diameter / 1000.0) - 2.5 * math.log10(self.albedo)
+        H = 15.618 - 5 * math.log10(self.diameter / 1000) - 2.5 * math.log10(self.albedo)
+        print(f"D = {self.diameter}, H = {H}")
         return H
+    
+    def phase_function(self, angle: float):
+        phi1 = math.exp(-3.33 * (math.tan(math.radians(angle / 2))) ** 0.63)
+        phi2 = math.exp(-1.87 * (math.tan(math.radians(angle / 2))) ** 1.22)
+
+        phase_term = (1 - self.G) * phi1 + self.G * phi2
+
+        return phase_term
+
+    
+    def apparent_magnitude(self, r: float, delta: float, phase_angle: float) -> float:
+
+        phi = Asteroid(self.G).phase_function(phase_angle)
+
+        m = self.H + 5 * math.log10(r * delta) - 2.5 * math.log10(phi)
+
+        return m
+  
+    
--- tests/test_asteroid.py
+++ tests/test_asteroid.py
@@ -2,7 +2,7 @@ import pytest
 
 from neo_detect.asteroid import Asteroid
 
-class Test_Abs_Mag:
+class Test_Asteroid:
 
     def test_diam_30m(self):
         expected_H = 25
@@ -19,4 +19,53 @@ class Test_Abs_Mag:
         H = ast.abs_mag()
 
         assert H == pytest.approx(expected_H, abs=0.1)
+
+    def test_angle_0(self):
+        expected_phi = 1.0
+
+        ast = Asteroid(30, 0.14, G=0.15)
+        phi = ast.phase_function(0.0)
+
+        assert phi == pytest.approx(expected_phi, abs=1e-3)
+
+    def test_angle_90(self):
+        expected_phi = 0.0535
+
+        ast = Asteroid(30, 0.14, G=0.15)
+        phi = ast.phase_function(90.0)
+
+        assert phi == pytest.approx(expected_phi, abs=1e-3)
+
+    def test_angle_180(self):
+        expected_phi = 0.0
+
+        ast = Asteroid(30, 0.14, G=0.15)
+        phi = ast.phase_function(180.0)
+
+        assert phi == pytest.approx(expected_phi, abs=1e-3)
+
+    def test_app_mag_15H_angle0(self):
+        expected_m = 15
+
+        ast = Asteroid(diameter=3600, albedo=0.14, G=0.15)
+        m = ast.apparent_magnitude(r = 1.0, delta = 1.0, phase_angle = 0.0)
+
+        assert m == pytest.approx(expected_m, abs = 0.1)
+
+    def test_app_mag_15H_angle30(self):
+        expected_m = 16.3
+
+        ast = Asteroid(diameter=3600, albedo=0.14, G=0.15)
+        m = ast.apparent_magnitude(r = 1.0, delta = 1.0, phase_angle = 30.0)
+
+        assert m == pytest.approx(expected_m, abs = 0.1)
+
+
+    def test_app_mag_30H_angle30(self):
+        expected_m = 31.3
+
+        ast = Asteroid(diameter=3.6, albedo=0.14, G=0.15)
+        m = ast.apparent_magnitude(r = 1.0, delta = 1.0, phase_angle = 30.0)
+
+        assert m == pytest.approx(expected_m, abs = 0.1)
     
