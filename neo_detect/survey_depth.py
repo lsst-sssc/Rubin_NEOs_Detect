58c74719c75c3019342afda7767d94e47d08f3b8
Dante Garcia <007046891@coyote.csusb.edu>
2026-07-22 12:44:23 -0700

    Added rubin-sim, changed files pyproject.toml and uv.lock to include dependency

--- neo_detect/survey_depth.py
+++ neo_detect/survey_depth.py
@@ -27,34 +27,3 @@ class Constant_Depth:
         """
         return self.m5_depth
     
-@dataclass
-class Rubin_Detection:
-    
-    def transform_Vmag(self, H: float, diameter: float, albedo: float) -> float:
-        """
-        Transforms the m5 r filter magnitude to V-band magnitude using the asteroid's absolute magnitude (H), diameter, and albedo.
-        
-        Parameters
-        ----------
-        H : float
-            The absolute magnitude of the asteroid.
-        diameter : float
-            The diameter of the asteroid.
-        albedo : float
-            The albedo of the asteroid.
-
-        Reference: Rubin Observatory 'Calculating LSST limiting magnitudes and SNR' https://smtn-002.lsst.io/
-
-        Returns
-        -------
-        float
-            The V-band magnitude of the asteroid.
-        """
-
-        # Compute the apparent magnitude in the r filter using the asteroid's absolute magnitude (H), diameter, and albedo
-        r_mag = H + 5 * np.log10(diameter / 1329) - 2.5 * np.log10(albedo)
-
-        # Transform the r filter magnitude to V-band magnitude using a color transformation
-        V_mag = r_mag + 0.44  # This is an approximate transformation; actual transformation may vary
-
-        return V_mag
