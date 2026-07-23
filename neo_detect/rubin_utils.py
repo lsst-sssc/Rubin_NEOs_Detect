c434d0283f2c8583a7f32017b22d237593173c66
Dante Garcia <007046891@coyote.csusb.edu>
2026-07-23 12:03:08 -0700

    Corrected rubin_utils and all tests passing.

--- neo_detect/rubin_utils.py
+++ neo_detect/rubin_utils.py
@@ -3,7 +3,7 @@ import os
 from rubin_scheduler.data import get_data_dir
 from rubin_sim.phot_utils import Bandpass, Sed
 
-def calc_colors(self, sedname='S.dat', filter_dir=None, sed_dir=None):
+def calc_colors(sedname='S.dat', filter_dir=None, sed_dir=None):
    """Calculate the colors for a given asteroids SED
 
    :param sedname: Name of the SED file, defaults to 'S.dat'
@@ -27,11 +27,13 @@ def calc_colors(self, sedname='S.dat', filter_dir=None, sed_dir=None):
    lsst = {}
    # TL: loop over filterlist and read each filter using Bandpass.read_throughput(os.path.join(filter_dir, f"{f}.dat"))
    for f in filterlist:
-      lsst[f] = Bandpass.read_throughput(os.path.join(filter_dir, f"{f}.dat"))
+      bp = Bandpass()
+      bp.read_throughput(os.path.join(filter_dir, f"total_{f}.dat"))
+      lsst[f] = bp
 
    # Read in V band filter
    # TL: set `v_dir` the way as sed_dir is set, then use vband.read_throughput(os.path.join(v_dir, v_filter))
-   v_dir = os.path.join(get_data_dir(), "throughputs/baseline") 
+   v_dir = os.path.join(get_data_dir(), "movingObjects") 
    # to read it (see read_filters() base_obs.py for details)
    v_filter = "harris_V.dat"
    v_band = Bandpass()
@@ -40,11 +42,11 @@ def calc_colors(self, sedname='S.dat', filter_dir=None, sed_dir=None):
 
    if sed_dir is None:
       sed_dir = os.path.join(get_data_dir(), "movingObjects")
+
    mo_sed = Sed()
    mo_sed.read_sed_flambda(os.path.join(sed_dir, sedname))
    # Calculate V magnitude using mo_sed.calc_mag(v_band)
    vmag = mo_sed.calc_mag(v_band)
-   self.colors[sedname] = {}
 
    # Loop over Rubin filters and calculate colors
    for f in filterlist:
